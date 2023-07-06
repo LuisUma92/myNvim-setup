4a06bea4ddd59670bdcfe0d69f14744cbbbf5fca
Luis Fernando Umaña Castro <luis.umanacastro@ucr.ac.cr>
2023-07-06 12:00:49 -0600

    Update localBib

---
+++ src/updateLocalBib.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#
+# This script update the local bib file using 
+# remote origin@github.com
+#
+
+# nvim ~/.config/mytex/bib/Biblioteca.bib                                 
+cp $MFPATH/opt/myNvim-setup/config/mytex/bib ~/.config/mytex/bib/Biblioteca.bib
